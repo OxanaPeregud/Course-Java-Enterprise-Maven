78a9f06eb6b91ef41b9fd7b922759bf10ffbb716
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-04-17 20:52:56 +0300

    rename main classes

--- src/main/java/com/peregud/Main.java
+++ src/main/java/com/peregud/Main.java
@@ -1,9 +1,7 @@
 package com.peregud;
 
-import com.peregud.resource_bundle.ResourceBundleStarter;
-
 public class Main {
     public static void main(String[] args) {
-        ResourceBundleStarter.start();
+
     }
 }
--- src/main/java/com/peregud/pattern_builder/PatternBuilderStarter.java
+++ src/main/java/com/peregud/pattern_builder/PatternBuilderStarter.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class PatternBuilderStarter {
     public static String filepath = String.valueOf(Paths.get("src", "main", "resources", "PersonList.txt"));
 
-    public static void main(String[] args) {
+    public static void start() {
         List<Person> list = PersonUtil.build(100);
         List<Person> personGenerateList = PersonGeneratorUtil.createPersonList(list);
         DisplayUtil.displayPersonList(personGenerateList);
