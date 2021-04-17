007cb299bcf5c840e150f362f67b17ba4a29fbbf
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-04-17 08:27:57 +0300

    add: sql input test

---
+++ src/test/java/com/peregud/database_sql_person/PersonSQLTest.java
@@ -0,0 +1,51 @@
+package com.peregud.database_sql_person;
+
+import org.junit.jupiter.api.Test;
+
+import java.sql.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonSQLTest {
+    private static final String SQL_URL = "jdbc:mysql://localhost:3306/list";
+
+    @Test
+    public void personInput() throws SQLException {
+        Connection conn = null;
+        try {
+            conn = DriverManager.getConnection(SQL_URL, "root", "1234");
+            Statement stmt = conn.createStatement();
+            conn.setAutoCommit(false);
+            stmt.executeUpdate("delete from list.person");
+
+            String sql = "insert into list.person(first_name, last_name, age) " + "VALUE (?, ?, ?);";
+            PreparedStatement preparedStatement = conn.prepareStatement(sql);
+            preparedStatement.setString(1, "John");
+            preparedStatement.setString(2, "Smith");
+            preparedStatement.setInt(3, 30);
+            preparedStatement.executeUpdate();
+
+            String firstName1 = "John";
+            String firstName2 = "Bob";
+            String lastName1 = "Smith";
+            String lastName2 = "Black";
+            int age1 = 30;
+            int age2 = 35;
+
+            try (ResultSet rs = stmt.executeQuery("select * from list.person")) {
+                assertTrue(rs.next());
+                assertEquals(firstName1, rs.getString("first_name"));
+                assertNotEquals(firstName2, rs.getString("first_name"));
+                assertEquals(lastName1, rs.getString("last_name"));
+                assertNotEquals(lastName2, rs.getString("last_name"));
+                assertEquals(age1, rs.getInt("age"));
+                assertNotEquals(age2, rs.getInt("age"));
+            }
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        } finally {
+            assert conn != null;
+            conn.rollback();
+        }
+    }
+}
