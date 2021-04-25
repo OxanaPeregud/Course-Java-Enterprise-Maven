6a36bb402b5e6ca105ae21e16f0b5fdd69f3cc91
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-04-25 10:42:42 +0300

    add: change data, delete

--- src/main/java/com/peregud/join_sql_db/Starter.java
+++ src/main/java/com/peregud/join_sql_db/Starter.java
@@ -10,10 +10,16 @@ public class Starter {
         SaveDataUtil.savePeople(CreatorUtil.createPeople());
         DAOPeople daoPeople = new DAOPeopleImpl();
         List<People> peopleFromDB = daoPeople.getAll();
+        ChangeDataUtil.changePersonAge(peopleFromDB, peopleFromDB.size(), 2);
+        ChangeDataUtil.changePersonAge(peopleFromDB, peopleFromDB.size() - 1, 2);
+        daoPeople.delete(1);
         DBView<People> dbPeopleView = new DBView<>();
         dbPeopleView.display(peopleFromDB);
         DAOAddress daoAddress = new DAOAddressImpl();
         List<Address> addressFromDB = daoAddress.getAll();
+        ChangeDataUtil.changeHouse(addressFromDB, addressFromDB.size(), 1);
+        ChangeDataUtil.changeHouse(addressFromDB, addressFromDB.size() - 1, 1);
+        daoAddress.delete(1);
         DBView<Address> dbAddressView = new DBView<>();
         dbAddressView.display(addressFromDB);
     }
