004b2d936e1e9d22811027a2dc21d9c378b5cad1
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-04-28 19:51:37 +0300

    fix: update last data from db

--- src/main/java/com/peregud/join_sql_db/Starter.java
+++ src/main/java/com/peregud/join_sql_db/Starter.java
@@ -1,22 +1,37 @@
 package com.peregud.join_sql_db;
 
+import com.peregud.join_sql_db.model.Address;
+import com.peregud.join_sql_db.model.Person;
+import com.peregud.join_sql_db.repository.DAOAddress;
+import com.peregud.join_sql_db.repository.DAOAddressImpl;
+import com.peregud.join_sql_db.repository.DAOPerson;
+import com.peregud.join_sql_db.repository.DAOPersonImpl;
 import com.peregud.join_sql_db.service.AddressService;
 import com.peregud.join_sql_db.service.PersonService;
 import com.peregud.join_sql_db.util.DBCreatorUtil;
 
+import java.sql.SQLException;
+import java.util.List;
+
 public class Starter {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         DBCreatorUtil.createPersonAddressDatabase();
         PersonService personService = new PersonService();
         AddressService addressService = new AddressService();
         personService.saveNewData();
         addressService.saveNewData();
 
-        personService.updateData(6, 2);
-        personService.updateData(7, 2);
+        DAOPerson daoPerson = new DAOPersonImpl();
+        DAOAddress daoAddress = new DAOAddressImpl();
 
-        addressService.updateData(4, 1);
-        addressService.updateData(5, 1);
+        List<Person> personFromDB = daoPerson.getAll();
+        List<Address> addressFromDB = daoAddress.getAll();
+
+        personService.updateData(personFromDB.size() - 1, 2);
+        personService.updateData(personFromDB.size(), 2);
+
+        addressService.updateData(addressFromDB.size() - 1, 1);
+        addressService.updateData(addressFromDB.size(), 1);
 
         personService.deleteData(1);
         addressService.deleteData(1);
