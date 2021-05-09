0c92769fd0c3fec8581bd559e8b219cf75fea83e
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-05-09 20:12:35 +0300

    add: test private constructor

--- src/test/java/com/peregud/dbprocedure/util/ReflectionUtilTest.java
+++ src/test/java/com/peregud/dbprocedure/util/ReflectionUtilTest.java
@@ -1,7 +1,11 @@
 package com.peregud.dbprocedure.util;
 
 import com.peregud.dbprocedure.model.Person;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Modifier;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -11,5 +15,19 @@ class ReflectionUtilTest {
     void annotatedField() {
         assertEquals("first_name", ReflectionUtil.annotatedField(Person.class, "firstName"));
         assertNotEquals("firstName", ReflectionUtil.annotatedField(Person.class, "firstName"));
+    }
+
+    @Test
+    public void testPrivateConstructor() {
+        Class<ReflectionUtil> clazz = null;
+        try {
+            clazz = ReflectionUtil.class;
+            Constructor<?>[] constructor = clazz.getDeclaredConstructors();
+            constructor[0].setAccessible(true);
+            constructor[0].newInstance();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        Assertions.assertFalse(Modifier.isPrivate(clazz.getModifiers()));
     }
 }
