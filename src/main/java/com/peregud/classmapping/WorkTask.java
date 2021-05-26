b38d0c00128c7cadc92e14541f2ddff47588da4f
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-05-26 19:29:25 +0300

    add: table per concrete class mapping

---
+++ src/main/java/com/peregud/classmapping/WorkTask.java
@@ -0,0 +1,19 @@
+package com.peregud.classmapping;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@EqualsAndHashCode(callSuper = true)
+@Data
+@NoArgsConstructor
+@SuperBuilder
+@Entity
+@Table(name = "work_task")
+public class WorkTask extends Task {
+    private double cost;
+}
