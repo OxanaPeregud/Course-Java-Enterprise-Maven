54b179fdcf464344291d8004f4569f0a707f42d4
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-06-03 21:04:42 +0300

    add tests

--- src/test/java/com/peregud/templatemethod/FinanceDepartmentSalaryCalculatorTest.java
+++ src/test/java/com/peregud/templatemethod/FinanceDepartmentSalaryCalculatorTest.java
@@ -2,15 +2,33 @@ package com.peregud.templatemethod;
 
 import org.junit.jupiter.api.Test;
 
-import static com.peregud.templatemethod.SalaryType.BASIC_SALARY;
+import static com.peregud.templatemethod.SalaryType.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class FinanceDepartmentSalaryCalculatorTest {
     FinanceDepartmentSalaryCalculator financeDepartmentSalaryCalculator = new FinanceDepartmentSalaryCalculator();
 
+    public void calculate() {
+        financeDepartmentSalaryCalculator.calculateBasicSalary();
+        financeDepartmentSalaryCalculator.calculateAdditionalSalary();
+        financeDepartmentSalaryCalculator.calculateMonthlyBonus();
+    }
+
     @Test
     void calculateBasicSalary() {
-        financeDepartmentSalaryCalculator.calculateBasicSalary();
+        calculate();
         assertEquals(10400.0, financeDepartmentSalaryCalculator.salary.get(BASIC_SALARY));
+    }
+
+    @Test
+    void calculateAdditionalSalary() {
+        calculate();
+        assertEquals(3120.0, financeDepartmentSalaryCalculator.salary.get(ADDITIONAL_SALARY));
+    }
+
+    @Test
+    void calculateMonthlyBonus() {
+        calculate();
+        assertEquals(2028.0, financeDepartmentSalaryCalculator.salary.get(MONTHLY_BONUS));
     }
 }
