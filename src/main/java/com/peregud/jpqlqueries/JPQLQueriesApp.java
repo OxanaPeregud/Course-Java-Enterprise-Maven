7326040f304832157cb1beb7a1795f06d2854bb3
Oxana Peregud <76512695+OxanaPeregud@users.noreply.github.com>
2021-06-10 08:44:19 +0300

    add: select queries

--- src/main/java/com/peregud/jpqlqueries/JPQLQueriesApp.java
+++ src/main/java/com/peregud/jpqlqueries/JPQLQueriesApp.java
@@ -4,6 +4,8 @@ import com.peregud.jpqlqueries.util.DBUtil;
 import com.peregud.jpqlqueries.util.DataGeneratorUtil;
 import com.peregud.jpqlqueries.util.HibernateUtil;
 
+import java.time.LocalDate;
+
 public class JPQLQueriesApp {
     public static void main(String[] args) {
         try {
@@ -12,6 +14,9 @@ public class JPQLQueriesApp {
             DBUtil.selectAll("Salesperson").forEach(System.out::println);
             System.out.println(DBUtil.selectColumn("name", "Store"));
             System.out.println(DBUtil.selectColumn("lastName", "Salesperson"));
+            System.out.println(DBUtil.selectSalesperson(1000));
+            System.out.println(DBUtil.selectSalesperson(LocalDate.of(2010, 6, 6)));
+            System.out.println(DBUtil.selectAllStores("Иванов", "Петров", "Сидоров"));
 
         } catch (Exception e) {
             e.printStackTrace();
--- src/main/java/com/peregud/jpqlqueries/util/DBUtil.java
+++ src/main/java/com/peregud/jpqlqueries/util/DBUtil.java
@@ -1,7 +1,12 @@
 package com.peregud.jpqlqueries.util;
 
+import com.peregud.jpqlqueries.model.FirstLastNames;
+import com.peregud.jpqlqueries.model.LastNamesSalary;
 import lombok.experimental.UtilityClass;
 
+import javax.persistence.Query;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @UtilityClass
@@ -9,13 +14,63 @@ public class DBUtil {
 
     public List<?> selectAll(String table) {
         return HibernateUtil.createEntityManager()
-                .createQuery("SELECT e FROM " + table + " e")
+                .createQuery("SELECT s FROM " + table + " s")
                 .getResultList();
     }
 
     public List<?> selectColumn(String field, String table) {
         return HibernateUtil.createEntityManager()
-                .createQuery("SELECT e." + field + " FROM " + table + " e")
+                .createQuery("SELECT s." + field + " FROM " + table + " s")
+                .getResultList();
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<?> selectSalesperson(double revenue) {
+        List<FirstLastNames> firstLastNames = new ArrayList<>();
+        List<Object[]> result;
+        Query query = HibernateUtil.createEntityManager()
+                .createQuery("SELECT s.firstName, s.lastName " +
+                        "FROM Salesperson s WHERE s.store.revenue > :revenue")
+                .setParameter("revenue", revenue);
+        result = query.getResultList();
+        for (Object[] salesperson : result) {
+            FirstLastNames firstLastName = new FirstLastNames();
+            String firstName = (String) salesperson[0];
+            String lastName = (String) salesperson[1];
+            firstLastName.setFirstName(firstName);
+            firstLastName.setLastName(lastName);
+            firstLastNames.add(firstLastName);
+        }
+        return firstLastNames;
+    }
+
+    @SuppressWarnings("unchecked")
+    public List<?> selectSalesperson(LocalDate localDate) {
+        List<LastNamesSalary> lastNamesSalaries = new ArrayList<>();
+        List<Object[]> result;
+        Query query = HibernateUtil.createEntityManager()
+                .createQuery("SELECT s.lastName, s.salary " +
+                        "FROM Salesperson s WHERE s.store.registrationDate < :localDate")
+                .setParameter("localDate", localDate);
+        result = query.getResultList();
+        for (Object[] salesperson : result) {
+            LastNamesSalary lastNamesSalary = new LastNamesSalary();
+            String lastName = (String) salesperson[0];
+            Double salary = (Double) salesperson[1];
+            lastNamesSalary.setLastName(lastName);
+            lastNamesSalary.setSalary(salary);
+            lastNamesSalaries.add(lastNamesSalary);
+        }
+        return lastNamesSalaries;
+    }
+
+    public List<?> selectAllStores(String lastName1, String lastName2, String lastName3) {
+        return HibernateUtil.createEntityManager()
+                .createQuery("SELECT distinct s.store.name FROM Salesperson s WHERE s.lastName " +
+                        "in (:lastName1, :lastName2, :lastName3)")
+                .setParameter("lastName1", lastName1)
+                .setParameter("lastName2", lastName2)
+                .setParameter("lastName3", lastName3)
                 .getResultList();
     }
 }
